e52114b4effc53831453c5927fa9f036ae85777c
MR_Geri <ilya.kamckine@ya.ru>
2021-09-26 02:19:19 +0300

    Опросник

--- questionnaire.py
+++ questionnaire.py
@@ -8,6 +8,7 @@ blueprint = Blueprint(
     template_folder='templates'
 )
 
+
 @blueprint.route('/questionnaire', methods=['GET', 'POST'])
 def questionnaire():
     if request.method == 'POST':
@@ -15,6 +16,6 @@ def questionnaire():
         select1 = request.form.get('2')
         select2 = request.form.get('3')
         select3 = request.form.get('4')
-        return("Прошел опрос? Молодец, возьми с полки пирожок)")
+        return "Прошел опрос? Молодец, возьми с полки пирожок)"
     elif request.method == 'GET':
         return render_template('questionnaire.html')
