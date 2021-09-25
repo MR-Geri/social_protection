45aaef9d6c2688ca1db8d0201ac39af22274832d
senya <senya.segeda@mail.ru>
2021-09-25 17:51:44 +0300

    base html

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/pomosh')
+def pomosh():
+    return render_template('pomosh.html')
+
+
 if __name__ == '__main__':
     app.run()
