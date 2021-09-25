3eed149f54724668cd1d06c7f3ebe89dd3933bd7
Pavel <working_pavel@mail.ru>
2021-09-25 20:19:57 +0300

    Handler /login using url_for() login.html

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Вход')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'social_protection_secret_key'
@@ -8,6 +17,12 @@ app.config['SECRET_KEY'] = 'social_protection_secret_key'
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('/'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/pomosh')
 def pomosh():
