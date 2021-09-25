f3f6895981ec064b8c31f40fadf21a2e98a0e799
MR_Geri <ilya.kamckine@ya.ru>
2021-09-26 02:29:06 +0300

    Edit

--- questionnaire.py
+++ questionnaire.py
@@ -8,6 +8,7 @@ blueprint = Blueprint(
     template_folder='templates'
 )
 
+
 @blueprint.route('/questionnaire', methods=['GET', 'POST'])
 def questionnaire():
     if request.method == 'POST':
@@ -15,7 +16,7 @@ def questionnaire():
         select1 = request.form.get('2')
         select2 = request.form.get('3')
         select3 = request.form.get('4')
-        data = "Вам пологаются соц. выплаты" #расчет льгот если они есть
+        data = "Вам пологаются соц. выплаты"  # расчет льгот если они есть
         return render_template('survey_result.html', data=data)
     elif request.method == 'GET':
         return render_template('questionnaire.html')
