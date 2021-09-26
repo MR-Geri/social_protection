f4cfb027112859b1878646daf79690f9edffee09
Pavel <working_pavel@mail.ru>
2021-09-26 09:51:25 +0300

    survey_result.html

--- questionnaire.py
+++ questionnaire.py
@@ -12,11 +12,22 @@ blueprint = Blueprint(
 @blueprint.route('/questionnaire', methods=['GET', 'POST'])
 def questionnaire():
     if request.method == 'POST':
-        select = request.form.get('1')
-        select1 = request.form.get('2')
+        data = []
+        select = request.form.get('1')[3:6]
+        if select[2] != '0':
+            income = int(select[0:2])
+        else:
+            income = int(select)
+        number_people = int(request.form.get('2'))
+        income /= number_people
+        if income < 10:
+            data.append("Ваш доход ниже прожиточного минимума, Вам пологается государственная помощь.")
         select2 = request.form.get('3')
-        select3 = request.form.get('4')
-        data = "Вам пологаются соц. выплаты"  # расчет льгот если они есть
+        if select2 == "да":
+            data.append("У Вас есть льготы")
+        select3 = request.form.get('4') #субъективная оценка своего состояния гражданином
+        if len(data) == 0:
+            data.append("У Вас нет льгот")
         return render_template('survey_result.html', data=data)
     elif request.method == 'GET':
         return render_template('questionnaire.html')
